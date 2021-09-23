903311186e776f3c190642026486f6c3ee791bde
mahidhar4 <mahidhar.kumar@gmail.com>
2021-09-23 12:13:12 +0530

    1 file modified

--- index.js
+++ index.js
@@ -3,4 +3,18 @@ import './style.css';
 
 // Write Javascript code!
 const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+appDiv.innerHTML = `<h1>JS Starter Please check below console in right side pane</h1>`;
+
+var prevConsoleLog = console.log;
+
+console.log = function () {
+  var newArgs = [];
+  for (var arg of arguments) {
+    newArgs = [...newArgs, arg, typeof arg];
+  }
+  prevConsoleLog.apply(this, newArgs);
+};
+
+console.log(3); // 3 "number"
+console.log('a'); // "a" "string"
+console.log(2, 'a'); // 2 "number" "a" "string"
