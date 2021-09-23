99aee02805d8fa93f5b4bf275cf4e115c49ffb45
mahidhar4 <mahidhar.kumar@gmail.com>
2021-09-23 13:14:43 +0530

    1 file modified

--- index.js
+++ index.js
@@ -18,3 +18,34 @@ console.log = function () {
 console.log(3); // 3 "number"
 console.log('a'); // "a" "string"
 console.log(2, 'a'); // 2 "number" "a" "string"
+
+function getValues(obj, rest) {
+  for (let item of Object.keys(obj)) {
+    if (!rest[obj[item]]) {
+      if (typeof obj[item] === 'object') {
+        getValues(obj[item], rest);
+      } else {
+        rest[obj[item]] = obj[item];
+      }
+    }
+  }
+  return Object.keys(rest).map((item) => parseInt(item));
+}
+
+function getUniqueSortedNumbers(obj) {
+  console.log(getValues(obj, {}));
+}
+
+getUniqueSortedNumbers({
+  a: {
+    x: 3,
+    y: {
+      d: 2,
+      e: 2,
+      f: {
+        g: 4,
+      },
+      z: 8,
+    },
+  },
+}); // [2, 3, 4, 8];
